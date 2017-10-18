cd2a703e6a0fe1bcbea100216131a2d25131f867
emilhvid <hvid2301@gmail.com>
2017-10-18 14:17:25 +0200

    before extracting httpAsync to class

--- Asay.mobile/app/src/main/java/asay/asaymobile/HttpAsyncTask.java
+++ Asay.mobile/app/src/main/java/asay/asaymobile/HttpAsyncTask.java
@@ -15,44 +15,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-/**
- * Created by s123725 on 17/10/2017.
- */
 
 public class HttpAsyncTask extends AsyncTask<String, Void, String> {
-    private JSONObject jsonObject;
-    private JSONArray jsonArray;
-    private String baseUrl = "http://oda.ft.dk/api/";
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
 
-    public JSONObject getJSONObject() {
-        Log.d("Getter", "getJSONObject: " + this.jsonObject.toString());
-        return this.jsonObject;
+        inputStream.close();
+        return result;
+
     }
 
-    public void setJSONObject(JSONObject value) {
-        this.jsonObject = value;
-    }
-
-    @Override
-    protected String doInBackground(String... urls) {
-
-        return GET(urls[0]);
-    }
-
-    // onPostExecute displays the results of the AsyncTask.
-    @Override
-    protected void onPostExecute(String result) {
-//            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-        try {
-            JSONObject object = new JSONObject(result);
-            Log.d("JSON", "onPostExecute: " + object.toString());
-            setJSONObject(object);
-        } catch (Exception exc) {
-            Log.d("JsonException", "Exception: " + exc.getMessage());
-        }
-    }
-
-    private String GET(String url) {
+    public static String GET(String url) {
         InputStream inputStream = null;
         String result = "";
         try {
@@ -79,16 +56,32 @@ public class HttpAsyncTask extends AsyncTask<String, Void, String> {
         return result;
     }
 
-    private String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while ((line = bufferedReader.readLine()) != null)
-            result += line;
+    @Override
+    protected String doInBackground(String... urls) {
 
-        inputStream.close();
-        return result;
+        return GET(urls[0]);
+    }
 
+    // onPostExecute displays the results of the AsyncTask.
+    @Override
+    protected void onPostExecute(String result) {
+//            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+        try {
+            JSONObject json = new JSONObject(result);
+            String str = "";
+
+            JSONArray articles = json.getJSONArray("value"); // get articles array
+
+            str += "articles length = " + json.getJSONArray("value").length();
+            str += "\n--------\n";
+            str += "names: " + articles.getJSONObject(0).names();
+//                str += "\n--------\n";
+//                str += "url: "+articles.getJSONObject(0).getString("url");
+
+//                etResponse.setText(str);
+
+        } catch (Exception exc) {
+//                etResponse.setText("Exception: " + exc.getMessage());
+        }
     }
 }
-
--- Asay.mobile/app/src/main/java/asay/asaymobile/MainActivity.java
+++ Asay.mobile/app/src/main/java/asay/asaymobile/MainActivity.java
@@ -3,24 +3,9 @@ package asay.asaymobile;
 import android.app.Activity;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class MainActivity extends Activity {
 
@@ -54,44 +39,6 @@ public class MainActivity extends Activity {
         new HttpAsyncTask().execute(baseUrl + call);
     }
 
-    public static String GET(String url){
-        InputStream inputStream = null;
-        String result = "";
-        try {
-
-            // create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
-
-            // make GET request to the given URL
-            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
-
-            // receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
-
-            // convert inputstream to string
-            if(inputStream != null)
-                result = convertInputStreamToString(inputStream);
-            else
-                result = "Did not work!";
-
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
-
-        return result;
-    }
-
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
-
-        inputStream.close();
-        return result;
-
-    }
 
     public boolean isConnected(){
         try {
@@ -108,33 +55,4 @@ public class MainActivity extends Activity {
         return false;
     }
 
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-
-            return GET(urls[0]);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-            try{
-                JSONObject json = new JSONObject(result);
-                String str = "";
-
-                JSONArray articles = json.getJSONArray("value"); // get articles array
-
-                str += "articles length = "+json.getJSONArray("value").length();
-                str += "\n--------\n";
-                str += "names: "+articles.getJSONObject(0).names();
-//                str += "\n--------\n";
-//                str += "url: "+articles.getJSONObject(0).getString("url");
-
-                etResponse.setText(str);
-
-            } catch (Exception exc){
-                etResponse.setText("Exception: " + exc.getMessage());
-            }
-        }
-    }
-}
+ }
