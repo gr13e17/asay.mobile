c5cad2748bacdc508ffac49607fde5c174dac61b
freyahelstrup <s165232@student.dtu.dk>
2017-11-23 22:23:15 +0100

    Made BillsFragment use same TabLayout as BillActivity

--- app/src/main/java/asay/asaymobile/fragments/BillsFragment.java
+++ app/src/main/java/asay/asaymobile/fragments/BillsFragment.java
@@ -1,8 +1,11 @@
 package asay.asaymobile.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,28 +14,87 @@ import asay.asaymobile.R;
 
 
 public class BillsFragment extends Fragment {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private BillsFragment.SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
     public BillsFragment() {
         // Required empty public constructor
     }
-    private FragmentTabHost mTabHost;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mTabHost = new FragmentTabHost(getActivity());
-        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.realtabcontent);
+        View view = inflater.inflate(R.layout.fragment_bills,container,false);
+        TabLayout tabLayout = view.findViewById(R.id.tabs);
 
-        mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("Alle"),
-                BillsAllFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("Mine"),
-                BillsAllFragment.class, null);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
 
-        // Inflate the layout for this fragment
-        return mTabHost;
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = view.findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        return view;
     }
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mTabHost = null;
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    BillsAllFragment billsAllFragment = new BillsAllFragment();
+                    return billsAllFragment;
+                case 1:
+                    //TODO: return billsMineFragment
+                    BillsAllFragment billsAllFragment2 = new BillsAllFragment();
+                    return billsAllFragment2;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Alle";
+                case 1:
+                    return "Mine";
+
+            }
+            return null;
+        }
     }
 }
