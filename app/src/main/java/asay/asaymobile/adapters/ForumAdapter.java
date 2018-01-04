de800eef33bf1dcabda768fe1612a6c377193a36
Unknown <mathias.s.larsen99@gmail.com>
2018-01-04 16:09:16 +0100

    moved adabtor to inner class in forum

--- app/src/main/java/asay/asaymobile/adapters/ForumAdapter.java
+++ app/src/main/java/asay/asaymobile/adapters/ForumAdapter.java
@@ -21,97 +21,98 @@ import asay.asaymobile.presenter.ForumPresenter;
  * Created by s123725 on 30/12/2017.
  */
 
-public class ForumAdapter extends BaseAdapter implements View.OnClickListener {
-    ArrayList<CommentDTO> currentComments;
-    ArrayList<UserDTO> currentUsers;
-    Context context;
-    LayoutInflater mInflater;
-    public View.OnClickListener listener;
-    ForumPresenter presenter;
-
-
-    public ForumAdapter(ArrayList<CommentDTO> currentComments, ArrayList<UserDTO> currentUsers, Context context, ForumPresenter presenter){
-        this.currentComments = currentComments;
-        this.currentUsers = currentUsers;
-        this.context = context;
-        this.mInflater = LayoutInflater.from(this.context);
-        this.presenter = presenter;
-    }
-
-    public void setButtonListener(View.OnClickListener listener) {
-        this.listener = listener;
-    }
-
-    private Integer getColor(ArgumentType argumentType){
-        switch (argumentType) {
-            case FOR:
-                return context.getResources().getColor(R.color.forColor);
-            case AGAINST:
-                return context.getResources().getColor(R.color.againstColor);
-            case NEUTRAL:
-                return context.getResources().getColor(R.color.neutralColor);
-            default:
-                return context.getResources().getColor(R.color.neutralColor);
-        }
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            convertView = inflater.inflate(R.layout.list_item_comment, parent, false);
-        }
-        final CommentDTO currentComment = currentComments.get(position);
-        TextView commentText = convertView.findViewById(R.id.comment);
-        commentText.setText(currentComment.getText());
-        TextView nameView = convertView.findViewById(R.id.nameView);
-        ImageButton upvote = (ImageButton) convertView.findViewById(R.id.up);
-        upvote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("you pressed upvote");
-                int score = currentComment.getScore();
-                currentComment.setScore(score +1);
-                presenter.updateComment(currentComment);
-            }
-        });
-        ImageButton downvote = (ImageButton) convertView.findViewById(R.id.down);
-        downvote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("you pressed downVote");
-                int score = currentComment.getScore();
-                currentComment.setScore(score -1);
-                presenter.updateComment(currentComment);
-            }
-        });
-        for (UserDTO user: currentUsers ){
-            if(user.getid() == currentComment.getUserid()){
-                nameView.setText(user.getname());
-                nameView.setBackgroundColor(getColor(currentComment.getArgumentType()));
-            }
-        }
-
-        return convertView;
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
-
-    @Override
-    public int getCount() {
-        return currentComments.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return currentComments.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-}
+//public class ForumAdapter extends BaseAdapter implements View.OnClickListener {
+//    ArrayList<CommentDTO> currentComments;
+//    ArrayList<UserDTO> currentUsers;
+//    Context context;
+//    LayoutInflater mInflater;
+//    public View.OnClickListener listener;
+//    ForumPresenter presenter;
+//
+//
+//    public ForumAdapter(ArrayList<CommentDTO> currentComments, ArrayList<UserDTO> currentUsers, Context context, ForumPresenter presenter){
+//        this.currentComments = currentComments;
+//        this.currentUsers = currentUsers;
+//        this.context = context;
+//        this.mInflater = LayoutInflater.from(this.context);
+//        this.presenter = presenter;
+//    }
+//
+//    public void setButtonListener(View.OnClickListener listener) {
+//        this.listener = listener;
+//    }
+//
+//    private Integer getColor(ArgumentType argumentType){
+//        switch (argumentType) {
+//            case FOR:
+//                return context.getResources().getColor(R.color.forColor);
+//            case AGAINST:
+//                return context.getResources().getColor(R.color.againstColor);
+//            case NEUTRAL:
+//                return context.getResources().getColor(R.color.neutralColor);
+//            default:
+//                return context.getResources().getColor(R.color.neutralColor);
+//        }
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        if (convertView == null) {
+//            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+//            convertView = inflater.inflate(R.layout.list_item_comment, parent, false);
+//        }
+//        final CommentDTO currentComment = currentComments.get(position);
+//        TextView commentText = convertView.findViewById(R.id.comment);
+//        commentText.setText(currentComment.getText());
+//        TextView nameView = convertView.findViewById(R.id.nameView);
+//        ImageButton upvote = (ImageButton) convertView.findViewById(R.id.up);
+//        upvote.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("you pressed upvote");
+//                int score = currentComment.getScore();
+//                currentComment.setScore(score +1);
+//                presenter.updateComment(currentComment);
+//            }
+//        });
+//        ImageButton downvote = (ImageButton) convertView.findViewById(R.id.down);
+//        downvote.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("you pressed downVote");
+//                int score = currentComment.getScore();
+//                currentComment.setScore(score -1);
+//                presenter.updateComment(currentComment);
+//            }
+//        });
+//        for (UserDTO user: currentUsers ){
+//            if(user.getid() == currentComment.getUserid()){
+//                nameView.setText(user.getname());
+//                nameView.setBackgroundColor(getColor(currentComment.getArgumentType()));
+//            }
+//        }
+//
+//        return convertView;
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return currentComments.size();
+//    }
+//
+//    @Override
+//    public Object getItem(int position) {
+//        return currentComments.get(position);
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//}
+//
--- app/src/main/java/asay/asaymobile/fragments/BillForumFragment.java
+++ app/src/main/java/asay/asaymobile/fragments/BillForumFragment.java
@@ -13,11 +13,13 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -26,7 +28,6 @@ import asay.asaymobile.ForumContract;
 import asay.asaymobile.R;
 import asay.asaymobile.activities.BillActivity;
 import asay.asaymobile.activities.MainActivity;
-import asay.asaymobile.adapters.ForumAdapter;
 import asay.asaymobile.model.ArgumentType;
 import asay.asaymobile.model.CommentDTO;
 import asay.asaymobile.model.UserDTO;
@@ -104,7 +105,6 @@ public class BillForumFragment extends Fragment implements ForumContract.View, V
         this.nameArray = users;
     }
 
-//////////////////////
 
     public void openBottomSheet (View v) {
 
@@ -141,6 +141,112 @@ public class BillForumFragment extends Fragment implements ForumContract.View, V
             );
             forumPresenter.addNewComment(comment);
             mBottomSheetDialog.dismiss();
+        }
+    }
+
+
+    /////////////////
+
+
+    public class ForumAdapter extends BaseAdapter implements View.OnClickListener {
+        ArrayList<CommentDTO> currentComments;
+        ArrayList<UserDTO> currentUsers;
+        Context context;
+        LayoutInflater mInflater;
+        public View.OnClickListener listener;
+        ForumPresenter presenter;
+
+
+        public ForumAdapter(ArrayList<CommentDTO> currentComments, ArrayList<UserDTO> currentUsers, Context context, ForumPresenter presenter){
+            this.currentComments = currentComments;
+            this.currentUsers = currentUsers;
+            this.context = context;
+            this.mInflater = LayoutInflater.from(this.context);
+            this.presenter = presenter;
+        }
+
+        public void setButtonListener(View.OnClickListener listener) {
+            this.listener = listener;
+        }
+
+        private Integer getColor(ArgumentType argumentType){
+            switch (argumentType) {
+                case FOR:
+                    return context.getResources().getColor(R.color.forColor);
+                case AGAINST:
+                    return context.getResources().getColor(R.color.againstColor);
+                case NEUTRAL:
+                    return context.getResources().getColor(R.color.neutralColor);
+                default:
+                    return context.getResources().getColor(R.color.neutralColor);
+            }
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                convertView = inflater.inflate(R.layout.list_item_comment, parent, false);
+            }
+            final CommentDTO currentComment = currentComments.get(position);
+            TextView commentText = convertView.findViewById(R.id.comment);
+            commentText.setText(currentComment.getText());
+            TextView nameView = convertView.findViewById(R.id.nameView);
+            ImageButton upvote = (ImageButton) convertView.findViewById(R.id.up);
+            upvote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println("you pressed upvote");
+                    int score = currentComment.getScore();
+                    currentComment.setScore(score +1);
+                    presenter.updateComment(currentComment);
+                }
+            });
+            ImageButton downvote = (ImageButton) convertView.findViewById(R.id.down);
+            downvote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println("you pressed downVote");
+                    int score = currentComment.getScore();
+                    currentComment.setScore(score -1);
+                    presenter.updateComment(currentComment);
+                }
+            });
+            for (UserDTO user: currentUsers ){
+                if(user.getid() == currentComment.getUserid()){
+                    nameView.setText(user.getname());
+                    nameView.setBackgroundColor(getColor(currentComment.getArgumentType()));
+                }
+            }
+            Button replyToComment = convertView.findViewById(R.id.replyToComment);
+            replyToComment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openBottomSheet(rootView);
+                }
+            });
+
+            return convertView;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return currentComments.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return currentComments.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
         }
     }
 
