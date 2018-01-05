501cd777c5f7894e5f83a3fe797e2939d916b4c1
freyahelstrup <s165232@student.dtu.dk>
2018-01-05 09:16:33 +0100

    Moved ProfileActivity to activities package

--- app/src/main/java/asay/asaymobile/activities/MainActivity.java
+++ app/src/main/java/asay/asaymobile/activities/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import asay.asaymobile.ProfileActivity;
 import asay.asaymobile.R;
 import asay.asaymobile.fragments.BillsFragment;
 
--- app/src/main/java/asay/asaymobile/activities/ProfileActivity.java
+++ app/src/main/java/asay/asaymobile/activities/ProfileActivity.java
@@ -1,10 +1,12 @@
-package asay.asaymobile;
+package asay.asaymobile.activities;
 
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import asay.asaymobile.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
