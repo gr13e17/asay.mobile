f3c844408677c6562b03449a77e7f0545b5bb515
freyahelstrup <s165232@student.dtu.dk>
2018-01-10 20:33:54 +0100

    Cleanup in VoteActivity

--- app/src/main/java/asay/asaymobile/activities/VoteActivity.java
+++ app/src/main/java/asay/asaymobile/activities/VoteActivity.java
@@ -4,19 +4,14 @@ package asay.asaymobile.activities;
  * Created by Ber on 02/11/2017.
  */
 
-import android.os.Build;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,115 +22,95 @@ import asay.asaymobile.model.ArgumentType;
 import asay.asaymobile.model.BillDTO;
 import asay.asaymobile.presenter.BillPresenter;
 
+public class VoteActivity extends AppCompatActivity implements BillContract.View {
+    private BillDTO bill;
+    private final BillDTO.Vote vote = new BillDTO.Vote();
+    private TextView status;
+    private Button buttonCancel;
 
-public class VoteActivity extends AppCompatActivity implements View.OnClickListener, BillContract.View {
-
-    PopupWindow votedWindow;
-    PopupWindow cancelWindow;
-    ConstraintLayout mainLayout;
-    BillPresenter presenter;
-    BillDTO bill;
-    BillDTO.Vote vote = new BillDTO.Vote();
-    TextView title;
-    TextView voted;
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+
         bill = getIntent().getExtras().getParcelable("bill");
-        presenter = new BillPresenter(this);
-        mainLayout = (ConstraintLayout) findViewById(R.id.activity_vote);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
-
-        //Creates 2 popup windows
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-
-        View votedView = inflater.inflate(R.layout.dialog_vote_complete, null);
-        votedWindow = new PopupWindow(votedView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
-        View cancelView = inflater.inflate(R.layout.dialog_cancel_vote, null);
-        cancelWindow = new PopupWindow(cancelView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
-
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            votedWindow.setElevation(15);
-            cancelWindow.setElevation(15);
-        } else {
-            votedWindow.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.dialog_holo_light_frame));
-            cancelWindow.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.dialog_holo_light_frame));
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
         }
 
-        //creates all buttons
+        buttonCancel = findViewById(R.id.buttonCancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelVote();
+            }
+        });
 
-        Button b1 = (Button) findViewById(R.id.buttonCancel);
-        Button b2 = (Button) findViewById(R.id.buttonAgainst);
-        Button b3 = (Button) findViewById(R.id.buttonFor);
-        Button b4 = (Button) votedView.findViewById(R.id.buttonOk);
-        Button b5 = (Button) cancelView.findViewById(R.id.buttonCancelNo);
-        Button b6 = (Button) cancelView.findViewById(R.id.buttonCancelYes);
-        b1.setOnClickListener(this);
-        b2.setOnClickListener(this);
-        b3.setOnClickListener(this);
-        b4.setOnClickListener(this);
-        b5.setOnClickListener(this);
-        b6.setOnClickListener(this);
+        findViewById(R.id.buttonAgainst).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vote(ArgumentType.AGAINST);
+            }
+        });
 
-        title = (TextView) findViewById(R.id.textView2);
-        ((TextView) findViewById(R.id.textView)).setText(bill.getNumber() + ": " + bill.getTitleShort());
-        voted = (TextView) findViewById(R.id.argForHead);
+        findViewById(R.id.buttonFor).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vote(ArgumentType.FOR);
+            }
+        });
 
+        ((TextView) findViewById(R.id.title)).setText(String.format("%s: %s", bill.getNumber(), bill.getTitleShort()));
+        status = findViewById(R.id.status);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
+    /**
+     * Shows vote registered dialog
+     */
+    private void vote(ArgumentType argumentType) {
+        vote.setVote(argumentType);
+        final BillPresenter presenter = new BillPresenter(this);
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.your_vote_is_registered)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        bill.addVote(vote);
+                        presenter.updateBill(bill);
+                        finish(); // close this activity and return to previous activity
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
 
-            case R.id.buttonCancel:
-                cancelWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
-                break;
-
-            case R.id.buttonOk:
-                votedWindow.dismiss();
-                bill.addVote(vote);
-                presenter.updateBill(bill);
-                finish();
-                break;
-
-            case R.id.buttonCancelYes:
-                cancelWindow.dismiss();
-                voted.setVisibility(view.INVISIBLE);
-                break;
-
-            case R.id.buttonCancelNo:
-                cancelWindow.dismiss();
-                break;
-
-            case R.id.buttonAgainst:
-                vote.setVote(ArgumentType.AGAINST);
-                votedWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
-                break;
-
-            case R.id.buttonFor:
-                vote.setVote(ArgumentType.FOR);
-                votedWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
-                break;
-
-            default:
-                break;
-        }
+    /**
+     * Shows cancel vote dialog
+     */
+    private void cancelVote() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.are_you_sure_you_want_to_cancel_vote)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        status.setVisibility(View.INVISIBLE);
+                        buttonCancel.setVisibility(View.INVISIBLE);
+                    }
+                })
+                .setNegativeButton(R.string.no, null)
+                .setCancelable(false)
+                .show();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // handle arrow click here
-        if (item.getItemId() == android.R.id.home) {
-            finish(); // close this activity and return to preview activity (if there is any)
-        }
-
+        if (item.getItemId() == android.R.id.home)
+            finish(); // close this activity and return to previous activity
         return super.onOptionsItemSelected(item);
     }
 
