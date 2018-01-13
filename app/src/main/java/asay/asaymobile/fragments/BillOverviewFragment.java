d1dc0e35931587298791c79755f6a158240c6112
freyahelstrup <s165232@student.dtu.dk>
2018-01-13 09:22:42 +0100

    Cleanup in BillOverviewFragment Fixing lots of warning

--- app/src/main/java/asay/asaymobile/fragments/BillOverviewFragment.java
+++ app/src/main/java/asay/asaymobile/fragments/BillOverviewFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,33 +27,31 @@ import asay.asaymobile.model.UserDTO;
 import asay.asaymobile.presenter.ForumPresenter;
 import asay.asaymobile.presenter.UserPresenter;
 
-public class BillOverviewFragment extends Fragment implements View.OnClickListener, ForumContract.View, UserContract.View{
-
-    TextView BillDesc;
-    String BillDescOrg;
-    TextView expBillDesc;
-    TextView arg1;
-    String arg1Org;
-    TextView expArg1;
-    TextView arg2;
-    String arg2Org;
-    TextView expArg2;
-    boolean isExpandedBillDesc  = false;
-    boolean isExpandedFor  = false;
-    boolean isExpandedAgainst  = false;
+public class BillOverviewFragment extends Fragment implements View.OnClickListener, ForumContract.View, UserContract.View {
+    private TextView BillDesc;
+    private String BillDescOrg;
+    private TextView expBillDesc;
+    private TextView arg1;
+    private String arg1Org;
+    private TextView expArg1;
+    private TextView arg2;
+    private String arg2Org;
+    private TextView expArg2;
+    private boolean isExpandedBillDesc = false;
+    private boolean isExpandedFor = false;
+    private boolean isExpandedAgainst = false;
     private BillDTO bill;
     private double userId = 1;
-    ForumPresenter presenter;
-    ImageButton sub;
-    Button vote;
-    boolean isSub = false;
+    private ForumPresenter presenter;
+    private ImageButton sub;
+    private Button vote;
+    private boolean isSub = false;
     private UserDTO user;
     private UserPresenter uPresenter;
 
     public BillOverviewFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -72,59 +71,57 @@ public class BillOverviewFragment extends Fragment implements View.OnClickListen
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bill_overview, container, false);
-
-
     }
 
     @Override
     public void onViewCreated(final View rootView, @Nullable Bundle savedInstanceState) {
-        expBillDesc = (TextView) rootView.findViewById(R.id.expandBillDesc);
+        expBillDesc = rootView.findViewById(R.id.expandBillDesc);
         expBillDesc.setOnClickListener(this);
 
-        BillDesc = (TextView) rootView.findViewById(R.id.billDesc);
+        BillDesc = rootView.findViewById(R.id.billDesc);
         BillDescOrg = bill.getResume();
         BillDesc.setText(BillDescOrg);
         BillDesc.setOnClickListener(this);
 
-        arg1 = (TextView) rootView.findViewById(R.id.argForTxt);
-       arg1.setMaxLines(3);
+        arg1 = rootView.findViewById(R.id.argForTxt);
+        arg1.setMaxLines(3);
 
-        arg2 = (TextView) rootView.findViewById(R.id.argAgainstTxt);
-       arg2.setMaxLines(3);
+        arg2 = rootView.findViewById(R.id.argAgainstTxt);
+        arg2.setMaxLines(3);
 
-        expArg1 = (TextView) rootView.findViewById(R.id.expandArgFor);
+        expArg1 = rootView.findViewById(R.id.expandArgFor);
         expArg1.setOnClickListener(this);
         expArg1.setVisibility(View.GONE);
 
-        expArg2 = (TextView) rootView.findViewById(R.id.expandArgAgainst);
+        expArg2 = rootView.findViewById(R.id.expandArgAgainst);
         expArg2.setOnClickListener(this);
         expArg2.setVisibility(View.GONE);
 
-        sub = (ImageButton) rootView.findViewById(R.id.subbtn);
+        sub = rootView.findViewById(R.id.subbtn);
         sub.setOnClickListener(this);
 
         uPresenter = new UserPresenter(this);
         uPresenter.getUser(userId);
         String billTitle = bill.getNumber().concat(": ").concat(bill.getTitleShort());
-        TextView header = (TextView) rootView.findViewById(R.id.headerBill);
+        TextView header = rootView.findViewById(R.id.headerBill);
         header.setText(billTitle);
-        vote = (Button) rootView.findViewById(R.id.buttonVote);
+        vote = rootView.findViewById(R.id.buttonVote);
         vote.setOnClickListener(this);
 
-        if (((AppCompatActivity) getActivity()).getSupportActionBar() != null)
-            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(billTitle);
-
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setTitle(billTitle);
 
         super.onViewCreated(rootView, savedInstanceState);
 
-                BillDesc.post(new Runnable() {
+        BillDesc.post(new Runnable() {
             @Override
             public void run() {
                 if (BillDesc.length() <= 0) {
                     BillDesc.setText(R.string.noDesc);
                 }
                 if (BillDesc.getLineCount() > 3) {
-                   addDots(BillDesc);
+                    addDots(BillDesc);
                     expBillDesc.setVisibility(View.VISIBLE);
 
                 } else
@@ -135,21 +132,19 @@ public class BillOverviewFragment extends Fragment implements View.OnClickListen
         });
     }
 
-
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+        switch (v.getId()) {
 
-            //BIll Description expanding
-            case R.id.billDesc :
-            case R.id.expandBillDesc :
-                if(isExpandedBillDesc){
+            // Bill Description expanding
+            case R.id.billDesc:
+            case R.id.expandBillDesc:
+                if (isExpandedBillDesc) {
                     collapseTextView(BillDesc, 3);
                     expBillDesc.setText(R.string.showMore);
                     isExpandedBillDesc = false;
                     BillDesc.setOnClickListener(this);
-                }
-                else {
+                } else {
                     expandTextView(BillDesc, BillDescOrg);
                     expBillDesc.setText(R.string.showLess);
                     isExpandedBillDesc = true;
@@ -158,15 +153,14 @@ public class BillOverviewFragment extends Fragment implements View.OnClickListen
                 break;
 
             // Top Argument Against Expanding
-            case R.id.argAgainstTxt :
-            case R.id.expandArgAgainst :
-                if(isExpandedAgainst){
+            case R.id.argAgainstTxt:
+            case R.id.expandArgAgainst:
+                if (isExpandedAgainst) {
                     expArg2.setText(R.string.showMore);
                     collapseTextView(arg2, 3);
                     isExpandedAgainst = false;
                     arg2.setOnClickListener(this);
-                }
-                else {
+                } else {
                     expandTextView(arg2, arg2Org);
                     expArg2.setText(R.string.showLess);
                     isExpandedAgainst = true;
@@ -175,71 +169,70 @@ public class BillOverviewFragment extends Fragment implements View.OnClickListen
                 break;
 
             // Top Argument For Expanding
-            case R.id.argForTxt :
-            case R.id.expandArgFor :
-
-                if(isExpandedFor){
+            case R.id.argForTxt:
+            case R.id.expandArgFor:
+                if (isExpandedFor) {
                     expArg1.setText(R.string.showMore);
                     collapseTextView(arg1, 3);
                     isExpandedFor = false;
                     arg1.setOnClickListener(this);
-                }
-                else {
+                } else {
                     expandTextView(arg1, arg1Org);
                     expArg1.setText(R.string.showLess);
                     isExpandedFor = true;
                     arg1.setOnClickListener(null);
                 }
                 break;
+
+            // Vote button
             case R.id.buttonVote:
                 Intent voteIntent = new Intent(this.getActivity(), VoteActivity.class);
                 voteIntent.putExtra("bill", bill);
                 startActivity(voteIntent);
                 break;
 
-            case R.id.subbtn :
-                toogleFavorite(sub);
+            // Add to favorites button
+            case R.id.subbtn:
+                toggleFavorite();
                 break;
 
         }
     }
 
-    private void addDots(TextView txt){
+    private void addDots(TextView txt) {
         int lineEndIndex = txt.getLayout().getLineEnd(2);
         String text;
-        if(txt.getText().toString().length() >= 3) {
-            text  = txt.getText().subSequence(0, lineEndIndex - 3) + "...";
+        if (txt.getText().toString().length() >= 3) {
+            text = txt.getText().subSequence(0, lineEndIndex - 3) + "...";
         } else
             text = "";
         txt.setText(text);
     }
 
-    private void expandTextView(TextView txt, String orgTxt){
+    private void expandTextView(TextView txt, String orgTxt) {
         txt.setText(orgTxt);
         ObjectAnimator animation = ObjectAnimator.ofInt(txt, "maxLines", txt.getLineCount());
-        if(txt.getLineCount()>7){
-        animation.setDuration(80).start();}
-        else{
+        if (txt.getLineCount() > 7) {
+            animation.setDuration(80).start();
+        } else {
             animation.setDuration(30).start();
         }
     }
 
-    private void collapseTextView(TextView txt, int numLines){
-
-        int lineEndIndex = txt.getLayout().getLineEnd(2);
+    private void collapseTextView(TextView txt, int numLines) {
         String text;
-        if(txt.getText().toString().length() >= 3) {
-          text  = txt.getText().subSequence(0, lineEndIndex - 3) + "...";
+        if (txt.getText().toString().length() >= 3) {
+            int lineEndIndex = txt.getLayout().getLineEnd(2);
+            text = txt.getText().subSequence(0, lineEndIndex - 3) + "...";
         } else {
             text = "";
         }
-
         txt.setText(text);
 
         ObjectAnimator animation = ObjectAnimator.ofInt(txt, "maxLines", numLines);
-        if(txt.getLineCount()>7){
-            animation.setDuration(80).start();}
-        else{
+        if (txt.getLineCount() > 7) {
+            animation.setDuration(80).start();
+        } else {
             animation.setDuration(30).start();
         }
     }
@@ -256,12 +249,12 @@ public class BillOverviewFragment extends Fragment implements View.OnClickListen
 
     @Override
     public void refreshCurrentCommentList(ArrayList<CommentDTO> comments) {
-        if(comments.size() != 0){
+        if (comments.size() != 0) {
             Integer maxFor = null, maxAgainst = null, positionFor = null, positionAgainst = null, counter = 0;
-            for(CommentDTO comment : comments){
+            for (CommentDTO comment : comments) {
                 int score = comment.getScore();
                 ArgumentType type = comment.getArgumentType();
-                if(type.equals(ArgumentType.FOR)){
+                if (type.equals(ArgumentType.FOR)) {
                     if (maxFor == null || maxFor < score) {
                         maxFor = comment.getScore();
                         positionFor = counter;
@@ -293,7 +286,7 @@ public class BillOverviewFragment extends Fragment implements View.OnClickListen
                     arg2Org = arg2.getText().toString();
                 }
             }
-        } else{ // No comments at all
+        } else { // No comments at all
             if (arg1 != null)
                 arg1.setText(R.string.noComments);
             if (arg2 != null)
@@ -319,37 +312,35 @@ public class BillOverviewFragment extends Fragment implements View.OnClickListen
                 expArg2.setVisibility(View.GONE);
             }
         }
-
-
     }
 
-    private void toogleFavorite(ImageButton button){
+    private void toggleFavorite() {
         ArrayList<Integer> billSaved = user.getbillsSaved();
 
-        if(isSub){
-            for(int i = billSaved.size() -1 ; i >= 0; i-- ){
-                if(billSaved.get(i).equals(bill.getId()))
+        if (isSub) {
+            for (int i = billSaved.size() - 1; i >= 0; i--) {
+                if (billSaved.get(i).equals(bill.getId()))
                     billSaved.remove(i);
             }
             sub.setImageResource(R.drawable.ic_star);
-        }else {
+        } else {
             billSaved.add(bill.getId());
             sub.setImageResource(R.drawable.ic_star_border);
         }
-        uPresenter.UpdateFavorites(userId,billSaved);
+        uPresenter.UpdateFavorites(userId, billSaved);
     }
 
     @Override
     public void refreshUser(UserDTO user) {
         System.out.println("number of bills saved: " + user.getbillsSaved().size());
         isSub = user.getbillsSaved().contains(bill.getId());
-        try{
+        try {
             if (isSub)
                 sub.setImageResource(R.drawable.ic_star);
-            else{
+            else {
                 sub.setImageResource(R.drawable.ic_star_border);
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         this.user = user;
@@ -359,6 +350,5 @@ public class BillOverviewFragment extends Fragment implements View.OnClickListen
     public void refreshUsers(ArrayList<UserDTO> users) {
 
     }
-
 
 }
